19baf8140bc84c2accc2c871d07836339be3faaa
Jarrod <Demonico@users.noreply.github.com>
2017-11-29 21:58:22 -0700

    created footer, about page and add story button

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const exphbs = require('express-handlebars');
 const mongoose =  require('mongoose');
 const cookieParser =  require('cookie-parser');
@@ -54,11 +55,13 @@ app.use((req, res, next) => {
   next();
 });
 
+//set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
 // Use Routes
 app.use('/', index)
 app.use('/auth', auth);
 app.use('/stories', stories);
-
 
 const port = process.env.PORT || 5000;
 
