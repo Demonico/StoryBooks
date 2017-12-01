a1daf72b754eb395ccab5604cef3c7c9af93611b
Jarrod <Demonico@users.noreply.github.com>
2017-12-01 14:10:51 -0700

    V 1.0.0 Alpha complete

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
-module.exports = {
-  mongoURI: 'mongodb://jdvd:hem258@ds123956.mlab.com:23956/storybooks-dev',
-  googleClientID: '368412823407-tna9bqj57bpskq1ca3c9bo08hervv4c9.apps.googleusercontent.com',
-  googleClientSecret: 'Je1wIL9VzhD-4cgZAcwuiR3s'
+if(process.env.NODE_ENV === 'production') {
+  module.exports = require('./keys_prod');
+} else {
+  module.exports = require('./keys_dev');
 }
